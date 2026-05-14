3dd1f97167dc83cf8c6a18ee948d38a57294eeca
RedPhoenix25 <timilehinmartins5@gmail.com>
2026-05-14 14:26:10 +0100

    Fix database connection path and config

--- server/database.js
+++ server/database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.resolve(__dirname, 'database.sqlite');
+const dbPath = path.join(process.cwd(), 'server', 'database.sqlite');
 const db = new sqlite3.Database(dbPath, sqlite3.OPEN_READONLY, (err) => {
   if (err) {
     console.error('Error opening database', err.message);
